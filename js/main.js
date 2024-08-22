b928e02216046ccb8efea15c19579219764e7048
Gurinder Sandhu <72296826+Guri-sandhu-codes@users.noreply.github.com>
2024-08-22 16:32:03 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -227,7 +227,7 @@
 			$.ajax({      	
 
 		      type: "POST",
-		      url: "inc/sendEmail.php",
+		      url: "send_email.php",
 		      data: $(form).serialize(),
 		      beforeSend: function() { 
 
