f58f2ba527d9ed9ee59547c274addc0761020ae0
Gurinder Sandhu <72296826+Guri-sandhu-codes@users.noreply.github.com>
2024-08-22 03:49:33 -0400

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,31 +1,51 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get POST data
-    $name = $_POST['contactName'];
-    $email = $_POST['contactEmail'];
-    $message = $_POST['contactMessage']) ;
-    $subject = $_POST['contactSubject'];
-    
-use Symfony\Component\Mailer\Mailer; 
-use Symfony\Component\Mailer\Transport\Smtp\SmtpTransport; 
-use Symfony\Component\Mime\Email;
+require 'vendor/autoload.php';
 
-require_once './vendor/autoload.php';
+$errors = [];
+$errorMessage = ' ';
+$successMessage = ' ';
+echo 'sending ...';
+if (!empty($_POST))
+{
+  $name = $_POST['contactName'];
+  $email = $_POST['contactEmail'];
+  $message = $_POST['ContactMessage'];
+  $subject = $_POST['ContactSubject'];
 
+  $fromEmail = 'anyname@example.com';
+      $emailSubject = 'New email from your contact form';
 
-$transport = (new Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport
-('sandbox.smtp.mailtrap.io', 587))
-                ->setUsername('fa17c29e7bf45a')
-                ->setPassword('1668347593e11d');
+      // Create a new PHPMailer instance
+      $mail = new PHPMailer(exceptions: true);
+      try {
+            // Configure the PHPMailer instance
+            $mail->isSMTP();
+            $mail->Host = 'live.smtp.mailtrap.io';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'api';
+            $mail->Password = '23ff03a5bfcacc22bdc1ad81edcf60b6';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+           
+            // Set the sender, recipient, subject, and body of the message 
+            $mail->setFrom($email);
+            $mail->addAddress($email);
+            $mail->setFrom($fromEmail);
+            $mail->Subject = $emailSubject;
+            $mail->isHTML( isHtml: true);
+            $mail->Body = "<p>Name: {$name}</p><p>Email: {$email}</p><p>Message: {$message}</p>";
+         
+            // Send the message
+            $mail->send () ;
+            $successMessage = "<p style='color: green; '>Thank you for contacting us :)</p>";
+      } catch (Exception $e) {
+            $errorMessage = "<p style='color: red; '>Oops, something went wrong. Please try again later</p>";
+echo $errorMessage;
+  }
+}
+}
 
-$mailer = new Mailer($transport); 
-
-$email = (new Email())
-            ->from($email, $name)
-            ->to('gurisandhu.codes@gmail.com')
-            ->subject($subject)
-            ->text($message)
-            
-
-$mailer->send($email);
+?>
